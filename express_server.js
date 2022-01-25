09b97357148ad8435cdc02103faa7eb3d83976d8
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-01-24 20:05:28 -0500

    Add edge case when shortURL is not found in database

--- express_server.js
+++ express_server.js
@@ -40,8 +40,13 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  if (urlDatabase[req.params.shortURL]) {
+    const longURL = urlDatabase[req.params.shortURL];
+    res.redirect(longURL);
+  }
+  else {
+    res.end("404 Not Found");
+  }
 });
 
 app.post("/urls", (req, res) => {
